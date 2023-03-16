8ab15428cdbe0eb1a91ab2a55fe6ae24bbf38a76
Smith Pereira Barros <126776412+SmithBarros23@users.noreply.github.com>
2023-03-16 09:29:17 -0300

    teste se um numero é primo

---
+++ Primo.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Primo {
+
+	public static void main(String[] args) {
+
+		//ler o numero inteiro do teclado
+		int n;
+		Scanner	scan = new Scanner(System.in);
+		System.out.print("Digite um número inteiro: ");
+		n = scan.nextInt();
+		scan.close();
+		//calcular se o número é primo
+		int contador = 2;
+		boolean achouDivisor = false;
+		while(contador < n && !achouDivisor){
+			if(n%contador == 0){
+				achouDivisor = true;
+			}else{
+				contador++;
+			}
+		}
+		//imprimir resultado
+		if(achouDivisor) {
+			System.out.println(n + " não é um número primo.");
+		}else{
+			System.out.println(n + " é um número primo.");
+		}
+		
+	}
+
+}
